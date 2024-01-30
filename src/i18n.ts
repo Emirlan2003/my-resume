ecca98ef772a2da1c48670b715de76227599d4a7
emir <emir03kgkg@gmail.com>
2024-01-31 05:21:04 +0600

    fix error

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import XHR from "i18next-xhr-backend";
-import { LocalStorageKey } from "@enums/localStorage";
+import { LocalStorageKey } from "./enums/localStorage";
 import { LocalStorage } from "./localStorage";
 
 i18n
